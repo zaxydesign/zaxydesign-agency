649d94de28efd80c226ad775454bb58d7adb8396
zaxydesign <zaxydesign.github@gmail.com>
2020-08-12 21:50:33 +0200

    Adding content

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1597330179;s:7:"authKey";s:64:"J@~=Vh{u*kJ9P%ufk3>r-D]Y)DP2IwBKbUpfG|M+8c7`(3JqX<D7;zHhqIq*pE:o";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2888;}
